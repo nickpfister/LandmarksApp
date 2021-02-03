7b3309f6d302e3d4151e333b3168b0ea94d171af
Nick Pfister <7969388+nickpfister@users.noreply.github.com>
2021-02-02 22:56:03 -0800

    Composing Complex Interfaces

--- LandmarksApp/LandmarksApp.swift
+++ LandmarksApp/LandmarksApp.swift
@@ -13,7 +13,7 @@ struct LandmarksApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(modelData)
         }
     }
 }
--- LandmarksApp/Views/Categories/CategoryHome.swift
+++ LandmarksApp/Views/Categories/CategoryHome.swift
@@ -2,7 +2,7 @@
 //  CategoryHome.swift
 //  LandmarksApp
 //
-//  Created by Nick Pfister on 1/5/21.
+//  Created by Nick Pfister on 2/2/21.
 //
 
 import SwiftUI
@@ -11,13 +11,20 @@ struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
     
     var body: some View {
-        NavigationView{
-            List{
-                ForEach(modelData.categories.keys.sorted(), id: \.self){key in
-                    Text(key)
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
             }
-                .navigationTitle("Featured")
+            .navigationTitle("Featured")
         }
     }
 }
